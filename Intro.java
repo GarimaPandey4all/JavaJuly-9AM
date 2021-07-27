1b3e55e627ef28181e48164777fccc7b359b7e40
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-07-27 11:49:54 +0530

    Add files via upload

--- Intro.java
+++ Intro.java
@@ -1,5 +1,13 @@
 class Intro {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
+
+        System.out.println("Hello World"); // print
+        System.out.print("Hello World");
+        System.out.println("Hello World");
+
+    }
+
+    public static void main(int args) {
         System.out.println("Brain Mentors");
     }
 }
