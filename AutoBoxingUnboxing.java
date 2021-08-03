adb165a1663de999213a9664212d83d4dc2939b3
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-03 20:01:27 +0530

    Add files via upload

---
+++ AutoBoxingUnboxing.java
@@ -0,0 +1,20 @@
+
+public class AutoBoxingUnboxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//Autoboxing
+		char ch = 'a';
+		Character a = ch; 
+		System.out.println(a);
+		
+		//Unboxing
+		Character b = 'b';
+		char c = b;
+		System.out.println(c);
+		
+	}
+
+}
