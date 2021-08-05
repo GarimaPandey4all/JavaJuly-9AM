75a0721d97cdec7ee89bb057263d3a35c779d08b
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-08-05 11:17:40 +0530

    Add files via upload

---
+++ LargestNumber.java
@@ -0,0 +1,23 @@
+
+public class LargestNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 20, b = 40, c = 60;
+		
+		if(a > b && a > c)
+		{
+			System.out.println(a);
+		}
+		else if(b > c)
+		{
+			System.out.println(b);
+		}
+		else {
+			System.out.println(c);
+		}
+
+	}
+
+}
---
+++ TernaryOperator.java
@@ -0,0 +1,15 @@
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 21;
+		
+		String result = (num % 2 == 0) ? "Number is Even" : "Number is Odd";
+		
+		System.out.println(result);
+
+	}
+
+}
